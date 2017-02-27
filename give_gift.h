209622d64d7e68ed02be0928f0779ab11915ae1a
root <root@localhost.localdomain>
2017-02-27 08:32:46 +0530

    final commit all Questions completed

--- give_gift.h
+++ give_gift.h
@@ -4,7 +4,9 @@
 #include "rel.h"
 #include <vector>
 class give_gift{
-	friend void giveGift(std::vector<gift> &g, std::vector<rel> &r); // friendly function so it can be accesed outside the class
+	public:
+		//friend keyword to make function usable by other classes
+		friend void giveGift(std::vector<gift> &g, std::vector<rel> &r); // friendly function so it can be accesed outside the class if not used gives function not declared error in other classes
 };
 void giveGift(std::vector<gift> &g, std::vector<rel> &r);
 #endif
--- make_rel.h
+++ make_rel.h
@@ -6,7 +6,8 @@
 #include <vector>
 class make_rel{
 	public:
-		friend void makeRel(std::vector<boy> &b, std::vector<girl> &g); //!friendly function so that can be used in other classes
+		//friend keyword to make function usable by all classes
+		friend void makeRel(std::vector<boy> &b, std::vector<girl> &g); //!friendly function so that can be used in other classes if not used gives function not declared error in other classes
 };
 void makeRel(std::vector<boy> &b, std::vector<girl> &g);
 #endif
